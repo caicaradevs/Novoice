14fc73281643dacff20b9a994de842039d27810f
Gabriel Gomes <gabrielogomes1@gmail.com>
2026-02-05 20:56:47 -0300

    first commit

---
+++ src/views/ContactView.vue
@@ -0,0 +1,14 @@
+<script></script>
+
+<template>
+  <main class="contact">
+    <h1>Contact Us</h1>
+    <p>If you have any questions, feel free to reach out!</p>
+  </main>
+</template>
+
+<style>
+.contact {
+  background-color: blue;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+
+    <form>
+      <label for="company-name">Nome da empresa:</label>
+      <input type="text" name="company-name" placeholder="Enter something..." />
+    </form>
+  </main>
+</template>
